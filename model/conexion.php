ebe954ce5daac361a148bc05cec6fb792e10ee95
jhonatanlituma1 <jslituma@espe.edu.ec>
2023-08-16 16:34:53 -0500

    DB

--- model/conexion.php
+++ model/conexion.php
@@ -1,14 +1,15 @@
 <?php 
-$contrasena = "root";
-$usuario = "root";
-$nombre_bd = "distribuidas";
+$host= getenv("MYSQL_SERVICE_HOST");
+$port= getenv("MYSQL_SERVICE_PORT");
+$database= getenv("MYSQL_DATABASE");
+$username= getenv("MYSQL_USER");
+$password= getenv("MYSQL_PASSWORD");
 
 try {
 	$bd = new PDO (
-		'mysql:host=34.121.105.224;
-		dbname='.$nombre_bd,
-		$usuario,
-		$contrasena,
+		'mysql:host='.$host.';dbname='.$database,
+		$username,
+		$password,
 		array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")
 	);
 } catch (Exception $e) {
